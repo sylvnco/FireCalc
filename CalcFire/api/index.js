645f80772196b8f4c1e0c3aeb5500723d1b2ec11
Sylvain Coulomb <sylvain.coulomb@gmail.com>
2021-09-07 19:59:03 +0200

    fix: update key

--- CalcFire/api/index.js
+++ CalcFire/api/index.js
@@ -1,6 +1,6 @@
 const MailerLite = require('mailerlite-api-v2-node').default
 
-const mailerLite = MailerLite('edd1c8cddfd55a3b27ec9bf0ad4a9d82Y')
+const mailerLite = MailerLite('edd1c8cddfd55a3b27ec9bf0ad4a9d82')
 
 
 export default (req, res) => {
