102c91a59c531abb43fddc40daaa7cc446831c02
Sylvain Coulomb <sylvain.coulomb@gmail.com>
2021-09-07 20:07:50 +0200

    add json response

--- CalcFire/api/index.js
+++ CalcFire/api/index.js
@@ -5,7 +5,11 @@ const mailerLite = MailerLite('edd1c8cddfd55a3b27ec9bf0ad4a9d82')
 
 export default (req, res) => {
     console.log(req);
-    mailerLite.addSubscriberToGroup(108014935, {
+    const result = mailerLite.addSubscriberToGroup(108014935, {
         email : req.body.email
+    }).then(() => {
+        res.json(true);
+    }, () => {
+        res.json(false);
     })
 }
